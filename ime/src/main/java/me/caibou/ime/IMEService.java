b44f0db28de3b79a02b2e85cd3bc9e64b8170335
Caibou <z.caibou@gmail.com>
2018-04-10 15:08:55 +0800

    Create rebuild branch

---
+++ ime/src/main/java/me/caibou/ime/IMEService.java
@@ -0,0 +1,9 @@
+package me.caibou.ime;
+
+import android.inputmethodservice.InputMethodService;
+
+/**
+ * @author caibou
+ */
+public class IMEService extends InputMethodService {
+}
---
+++ ime/src/main/java/me/caibou/ime/MainActivity.java
@@ -0,0 +1,33 @@
+package me.caibou.ime;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_select_input_method).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((InputMethodManager)MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).showInputMethodPicker();
+            }
+        });
+
+        findViewById(R.id.btn_setting_input_method).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+}
